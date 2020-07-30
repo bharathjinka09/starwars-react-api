038f28c0c0f99bed5a1ec61165d96ba47dfd89a2
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-30 16:01:09 +0530

    Pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
-import { useQuery } from 'react-query';
+import { usePaginatedQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async (key, greeting, page) => {
-  console.log(greeting)
+const fetchPlanets = async (key,  page) => {
   const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 }
 
 const Planets = () => {
   const [ page, setPage ] = useState(1)
-  const { data, status } = useQuery(['planets','hello',page], fetchPlanets, {
+  const {
+      resolvedData, 
+      latestData, 
+      status 
+   } = usePaginatedQuery(['planets',page], fetchPlanets, {
     staleTime: 0, 
     // cacheTime: 10,
     onSuccess: () => console.log('data fetched successfully')
   });
-  console.log(data);
 
   return (
     <div>
       <h2>Planets</h2>
-
-      <button onClick={() => setPage(1)}>page 1</button>
-      <button onClick={() => setPage(2)}>page 2</button>
-      <button onClick={() => setPage(3)}>page 3</button>
 
       {status === 'loading' && (
         <div>Loading data...</div>
@@ -34,9 +32,20 @@ const Planets = () => {
       )}
 
       {status === 'success' && (
-        <div>
-          { data.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
-        </div>
+        <>
+          <button
+            onClick={() => setPage(old => Math.max(old-1,1))}
+            disabled={page === 1}
+          >Previous page</button>
+          <span>{ page }</span>
+          <button
+            onClick={() => setPage(old => (!latestData || !latestData.next ? old : old + 1))}
+            disabled={!latestData || !latestData.next}
+          >Next page</button>
+          <div>
+            { resolvedData.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
+          </div>
+        </>
       )} 
     </div>
   );
